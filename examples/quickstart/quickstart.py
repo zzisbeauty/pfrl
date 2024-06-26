d560df28f1bf32a7901a72280c69ee4d6c92f473
giikinfanzhimin <swhardbewell@qq.com>
2024-06-26 16:10:32 +0800

    迁移至 notobook 运行

--- examples/quickstart/quickstart.py
+++ examples/quickstart/quickstart.py
@@ -13,16 +13,17 @@ obs = env.reset()
 print('initial observation:', obs)
 
 action = env.action_space.sample()
-obs, r, done, info = env.step(action)
+obs, r, done, info = env.step(action) # run action and change env status
 print('next observation:', obs)
 print('reward:', r)
 print('done:', done)
 print('info:', info)
 
 # Uncomment to open a GUI window rendering the current state of the environment
-# env.render()
+# env.render()  # return env status
 
 
+# Agent
 
 class QFunction(torch.nn.Module):
 
@@ -56,3 +57,63 @@ q_func2 = torch.nn.Sequential(
 
 # Use Adam to optimize q_func. eps=1e-2 is for stability.
 optimizer = torch.optim.Adam(q_func.parameters(), eps=1e-2)
+
+
+# Set the discount factor that discounts future rewards.
+gamma = 0.9
+
+# Use epsilon-greedy for exploration
+explorer = pfrl.explorers.ConstantEpsilonGreedy(
+    epsilon=0.3, random_action_func=env.action_space.sample)
+
+# DQN uses Experience Replay.
+# Specify a replay buffer and its capacity.
+replay_buffer = pfrl.replay_buffers.ReplayBuffer(capacity=10 ** 6)
+
+# Since observations from CartPole-v0 is numpy.float64 while
+# As PyTorch only accepts numpy.float32 by default, specify
+# a converter as a feature extractor function phi.
+phi = lambda x: x.astype(numpy.float32, copy=False)
+
+# Set the device id to use GPU. To use CPU only, set it to -1.
+gpu = -1
+
+# Now create an agent that will interact with the environment.
+agent = pfrl.agents.DoubleDQN(
+    q_func,
+    optimizer,
+    replay_buffer,
+    gamma,
+    explorer,
+    replay_start_size=500,
+    update_interval=1,
+    target_update_interval=100,
+    phi=phi,
+    gpu=gpu,
+)
+
+
+n_episodes = 300
+max_episode_len = 200
+for i in range(1, n_episodes + 1):
+    obs = env.reset()
+    R = 0  # return (sum of rewards)
+    t = 0  # time step
+    while True:
+        # Uncomment to watch the behavior in a GUI window
+        # env.render()
+        action = agent.act(obs)
+        obs, reward, done, _ = env.step(action)
+        R += reward
+        t += 1
+        reset = t == max_episode_len
+        agent.observe(obs, reward, done, reset)
+        if done or reset:
+            break
+    if i % 10 == 0:
+        print('episode:', i, 'R:', R)
+    if i % 50 == 0:
+        print('statistics:', agent.get_statistics())
+print('Finished.')
+
+
